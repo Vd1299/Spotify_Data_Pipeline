c46266f1edc2fa52e0fcc28510d6c207c2b0c0e0
Vd1299 <vishvesh.dumbre@gmail.com>
2024-10-06 11:35:22 -0700

    changed load and transform files

--- airflow_proj/dags/etl/load.py
+++ airflow_proj/dags/etl/load.py
@@ -2,7 +2,7 @@ import boto3
 from io import StringIO
 
 def load_to_s3(bucket_name, df, file_name, aws_access_key, aws_secret_key, aws_region):
-    # Create a session using the provided credentials
+
     s3 = boto3.client(
         's3',
         aws_access_key_id=aws_access_key,
@@ -10,14 +10,12 @@ def load_to_s3(bucket_name, df, file_name, aws_access_key, aws_secret_key, aws_r
         region_name=aws_region
     )
     
-    # Convert the DataFrame to a CSV buffer
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False)
-    
-    # Upload the CSV data to the specified S3 bucket
+
     s3.put_object(
         Bucket=bucket_name,
-        Key=f"processed/{file_name}",
+        Key=f"{file_name}",
         Body=csv_buffer.getvalue()
     )
     print(f"File {file_name} successfully uploaded to {bucket_name}.")
--- airflow_proj/dags/etl/transform.py
+++ airflow_proj/dags/etl/transform.py
@@ -7,21 +7,26 @@ def transform_csv(file_path):
     df = df.dropna()
     df = df.drop(columns=['cover_url', 'key', 'mode', 'in_deezer_playlists', 'in_deezer_charts', 'in_spotify_playlists', 'in_apple_playlists'])
 
-    # Convert percentage columns to decimal
     percentage_cols = ['danceability_%', 'valence_%', 'energy_%', 'acousticness_%', 
-                    'instrumentalness_%', 'liveness_%', 'speechiness_%']
+                       'instrumentalness_%', 'liveness_%', 'speechiness_%']
 
     for col in percentage_cols:
         df[col] = df[col] / 100.0
 
+    numeric_cols = ['streams']  
+    for col in numeric_cols:
+        if col in df.columns:
+            df[col] = df[col].str.replace(',', '', regex=False).astype(float)
+
     df['release_date'] = df.apply(
-    lambda row: pd.to_datetime(
-        f"{int(row['released_year'])}-{int(row['released_month'])}-{int(row['released_day'])}",
-        errors='coerce'
-    ), 
-    axis=1
+        lambda row: pd.to_datetime(
+            f"{int(row['released_year'])}-{int(row['released_month'])}-{int(row['released_day'])}",
+            errors='coerce'
+        ), 
+        axis=1
     )
 
     df = df.drop(columns=['released_year', 'released_month', 'released_day'])
     
     return df
+
